f1ff46ee60617130c173250a97625f3c3afb4cc9
Aamir Saleem <65162736+aamirsaleem1286@users.noreply.github.com>
2025-10-21 21:41:10 +0500

    push

--- src/helpers/sendResponse.js
+++
@@ -1,7 +0,0 @@
-export const sendResponse = (res, statusCode, error, errors, data) => {
-    res.status(statusCode).json({
-        error,
-        errors,
-        data
-    })
-}
--- src/lib/tokens/generate.token.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken'
-const generateAccessToken = (payload) => {
-    return jwt.sign(payload,process.env.AUTH_SECRET,{expiresIn:'1d'})
-}
-// const generateRefreshToken = (payload) => {
-//     return jwt.sign(payload,env.REFRESH_SECRET,{expiresIn:'7d'})
-// }
-export {generateAccessToken}
